deb59bcb165ba015d185987b948e4b0ec7001600
Sleyder Zuleta <sleyderzm1@gmail.com>
2017-06-08 17:26:37 -0500

    virified with hash

--- app.rb
+++ app.rb
@@ -1,11 +1,15 @@
 require 'rubygems'
+require 'json'
 require 'sinatra/base'
 
 class MyApp < Sinatra::Base
 
   post '/' do
     json = request.body.read
-    json.inspect
+    data = JSON.parse(json)
+    if data['type'] === 'url_verification'
+      data['challenge']
+    end
   end
 
 end
