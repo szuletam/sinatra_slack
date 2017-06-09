6de7f51afb2fee7786ea9d6c8f8e1d07ecbe1c44
Sleyder Zuleta <sleyderzm1@gmail.com>
2017-06-08 19:59:49 -0500

    BOT SEND MSG

--- app.rb
+++ app.rb
@@ -1,14 +1,36 @@
 require 'rubygems'
 require 'json'
+require 'slack'
 require 'sinatra/base'
 
 class MyApp < Sinatra::Base
 
+  TOKEN_AUTH = 'xoxp-191364520960-191364521120-194432731184-eff97d58cda967c8d430435231768785'
+
   post '/' do
+
     json = request.body.read
-    data = JSON.parse(json)
-    if data['type'] === 'url_verification'
-      data['challenge']
+    unless json.empty?
+
+      Slack.configure do |config|
+        config.token = TOKEN_AUTH
+      end
+      client = Slack::Web::Client.new
+      auth = client.auth_test
+      if auth['ok']
+        data = JSON.parse(json)
+        case data['type']
+          when 'channel_created'
+            msg = client.chat_postMessage(channel: data['channel']['id'], text: 'Hello Everyone!!', as_user: false, username: 'ultrabot')
+            'error sending msg' << msg['error'] unless msg['ok']
+          when 'url_verification'
+            data['challenge']
+          else
+            'error data'
+        end
+      else
+        'auth error' << auth['error']
+      end
     end
   end
 
